915236e776a6edb4fe6b4b9f96c22ef6341ad5ee
nicholascc <charettenr@gmail.com>
2020-10-16 11:00:03 -0500

    Error messages now display file where error occurred.

--- src/index.js
+++ src/index.js
@@ -11,14 +11,15 @@ const outPath = "./site";
 
 const statementRegex = /\$\<(.*?)\>\$/g;
 
-function err(message) {
-  if(colorEnabled) println(chalk.redBright("⚠ Error:", message));
-  else println("⚠ Error:", message);
+function err(message, file="") {
+  let fullMessage = "⚠ Error" + (file ? " in file \"" + file + "\"" : "") + ": " + message;
+  if(colorEnabled) println(chalk.redBright(fullMessage));
+  else println(fullMessage);
 }
 
-function assert(x, message) {
+function assert(x, message, file="") {
   if(!x) {
-    err(message);
+    err(message, file);
   }
   return !x;
 }
@@ -38,19 +39,19 @@ function updateSite() {
         evaluateTemplateCode: true ? x[1] === '$' : false
       }
     }).forEach(function (statement) {
-      fs.readFile(statement.inPath, function (err, data) {
-        if(err) {
-          throw err;
+      fs.readFile(statement.inPath, function (error, data) {
+        if(error) {
+          throw error;
         }
         data = data.toString('utf8');
 
         if(statement.evaluateTemplateCode) {
-          data = parseFile(data, statement);
+          data = parseFile(data, statement, statement.inPath);
           try {
             fs.ensureDirSync(path.dirname(statement.outPath));
             fs.writeFileSync(statement.outPath, data);
-          } catch (error) {
-            err(error);
+          } catch (error2) {
+            err(error2, statement.inPath);
           }
         } else {
           //println("Skipping evaluating template code in file", statement.inPath);
@@ -63,7 +64,7 @@ function updateSite() {
   // TODO: Delete left-over output directory contents.
 }
 
-function parseFile(data, evaluateTemplateCode, vars={}) {
+function parseFile(data, evaluateTemplateCode, filename, vars={}) {
   let parentPath = '';
   let currentlyFilling = '';
   let fillStartIndex = 0;
@@ -84,8 +85,8 @@ function parseFile(data, evaluateTemplateCode, vars={}) {
 
     switch(command) {
       case "inherit":
-        if(assert(args.length == 1, "One argument must be passed to 'inherit' command.") ||
-           assert(parentPath == '', "There cannot be more than one 'inherit' command in a file.")) {
+        if(assert(args.length == 1, "One argument must be passed to 'inherit' command.", filename) ||
+           assert(parentPath == '', "There cannot be more than one 'inherit' command in a file.", filename)) {
              return "";
            }
 
@@ -96,8 +97,8 @@ function parseFile(data, evaluateTemplateCode, vars={}) {
         finishFill(stmtStartIndex);
         fillStartIndex = stmtEndIndex;
 
-        if(assert(parentPath != '', "A parent path must be provided through an 'inherit' command before a 'fill' command can be used.") ||
-           assert(args.length == 1, "One argument must be passed to 'fill' command.")) {
+        if(assert(parentPath != '', "A parent path must be provided through an 'inherit' command before a 'fill' command can be used.", filename) ||
+           assert(args.length == 1, "One argument must be passed to 'fill' command.", filename)) {
              currentlyFilling = '';
              return "";
            }
@@ -112,7 +113,7 @@ function parseFile(data, evaluateTemplateCode, vars={}) {
   finishFill(data.length);
 
   if(parentPath) {
-    if(assert(valueDict, "At least one 'fill' command must be provided if an 'inherit' command is used.")) {
+    if(assert(valueDict, "At least one 'fill' command must be provided if an 'inherit' command is used.", filename)) {
       valueDict["."] = data; // @Incomplete: what's the best behavior when no 'fill' commands are provided? For now just using the entire input data as output.
     }
   } else {
@@ -127,19 +128,19 @@ function parseFile(data, evaluateTemplateCode, vars={}) {
 
       switch(command) {
         case "slot":
-          if(assert(args.length == 1, "One argument must be passed to 'slot' command.") ||
-             assert(args[0] in vars, "Variable '" + args[0] + "' must be set.")) {
+          if(assert(args.length == 1, "One argument must be passed to 'slot' command.", filename) ||
+             assert(args[0] in vars, "Variable '" + args[0] + "' must be set.", filename)) {
                return str; // @Incomplete: what's the best behavior when a slot command fails? For now just outputting the command itself as if it were raw text.
              }
           return vars[args[0]];
         default:
-          err("Unknown command " + command);
+          err("Unknown command " + command, filename);
           return str; // @Incomplete: what's the best behavior when a command is unknown? For now just outputting the command itself as if it were raw text.
       }
     });
   }
   if(parentPath) {
-    return parseFile(fs.readFileSync(parentPath).toString('utf8'), true, valueDict);
+    return parseFile(fs.readFileSync(parentPath).toString('utf8'), true, parentPath, valueDict);
   } else {
     return valueDict["."];
   }
